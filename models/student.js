84c62a104d9a3ef819de7120241426da77d9c74d
F R Summit <www.frsummit1@gmail.com>
2020-12-12 15:46:32 +0600

    uploading project

---
+++ models/student.js
@@ -0,0 +1,11 @@
+class Student {
+    constructor(id, name, phone, email, address) {
+        this.id = id;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.address = address;
+    }
+}
+
+module.exports = Student;
